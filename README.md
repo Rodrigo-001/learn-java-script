adcb8400f43aca5e33ca660136b09d1f3f92c076
rodrigoc01 <rodrigocamargo1234santos@gmail.com>
2023-11-30 11:10:49 -0300

    adicionado promise.all e async/await

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Revisão de conceitos em JavaScript
 
 Abordado:
-- Manipulação do DOM, eventos, promisses, json, funções etc.
+- Manipulação do DOM, eventos, promisses, json, funções, tratamento de exceptions etc.
 
 Resumindo, esta se trata de uma pasta com exercícios feitos em JS Vanila, html e Css.
--- modulo4 - encadeamento/script.js
+++ modulo4 - encadeamento/script.js
@@ -79,3 +79,64 @@ function obterGatinho() {
         });
 
 }
+
+// FUNÇÃO COM MULTIPLAS PROMISSES
+function promisseAll() {
+    resul = document.querySelector("#resul-nome");
+    const fetchPromise1 = fetch("https://api.thecatapi.com/v1/images/search");
+    const fetchPromise2 = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
+    const fetchPromise3 = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found");
+
+    //recebe um array de promisses e retorna uma nova promisse
+    Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+    .then((responses)=>{
+        for (const response of responses) {
+            resul.innerHTML +=`</br><strong>API:</strong>${response.url}<strong> | STATUS:</strong> ${response.status}`;
+        }
+    })
+    .catch((error)=>{
+        console.error(`Falha ao buscar${error}`);        
+    });
+    resul.innerHTML = `Neste exemplo faço uma requisição para 3 APIs diferentes, e exibo o status de cada uma na tela. </br>"O Promisse.all" é útil quando há <strong>Requisições paralelas em APIs, Operações assíncronas independentes, Validação simultânea</strong>. </br>Mas se precisar que qualquer uma delas sejam cumpridas o "Promisse.any" será mais útil.</br>`;
+}
+
+// FUNÇÃO COM PROMISSE ANY
+function promisseAny() {
+    resul = document.querySelector("#resul-nome");
+    const fetchPromise1 = fetch("https://api.thecatapi.com/v1/images/search");
+    const fetchPromise2 = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
+    const fetchPromise3 = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found");
+
+    //recebe um array de promisses e retorna uma nova promisse
+    Promise.any([fetchPromise1, fetchPromise2, fetchPromise3])
+    .then((response)=>{
+        resul.innerHTML +=`</br><strong>API:</strong>${response.url}<strong> | STATUS:</strong> ${response.status}`;
+    })
+    .catch((error)=>{
+        console.error(`Falha ao buscar${error}`);        
+    });
+    resul.innerHTML = `Neste exemplo faço uma requisição para 3 APIs diferentes, e exibo o status de qualquer uma delas que for cumprida. </br>"O Promisse.any" é útil quando há <strong>Requisições paralelas em APIs, Operações assíncronas independentes, Validação simultânea</strong>. </br>Mas se precisar que todas elas sejam cumpridas o "Promisse.all" será mais útil.</br>`;
+}
+
+// FUNÇÃO ASYNC AWAIT
+async function funcAsyncAwait() {
+    resul = document.querySelector("#resul-nome");
+
+    try {
+        // A função irá esperar que a chamada `fetch()` seja resolvida
+        const response = await fetch(
+          "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+        // A função irá esperar que a chamada `response.json()` seja resolvida
+        // a chamada `response.json()` retornará o objeto JSON processado ou gerará um erro
+        const data = await response.json();
+        resul.innerHTML = `RESPOSTA: ${data[0].name}`;
+        resul.innerHTML += `</br></br><p>Aqui estou uma função await, ela força operações assíncronas a serem concluídas em série. Isso é necessário se o resultado da próxima operação depender do resultado da última. Observe que esse texto aparece após a resposta, ao contrário das outras. </p>`;
+      } catch (error) {
+        console.error(`Não foi possível obter os produtos: ${error}`);
+      }
+  }
+  
